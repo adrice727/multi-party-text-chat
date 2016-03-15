4b3a0b20e6443867bead47982b8c994dfef688e7
Aaron Rice <adrice727@gmail.com>
2016-03-15 13:38:43 -0700

    getSessionData should return a promise.

--- public/js/api.js
+++ public/js/api.js
@@ -1,17 +1,7 @@
 var api = (function() {
 
-    var getToken = function() {
-
-        return axios.post('/getToken')
-            .then(function(res) {
-                window.sessionData = res.data;
-            }, function(err) {
-                console.log('Error: ', err);
-            });
-    };
-
     return {
-        getSessionData: getToken
+        getSessionData: function() { return axios.post('/getToken');}
     };
 
 })();
