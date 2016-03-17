e41762f0591f4cc8dee20dad6a3c747dd5779c4a
Aaron Rice <adrice727@gmail.com>
2016-03-17 15:27:44 -0700

    No promises anywhere.

--- public/js/api.js
+++ public/js/api.js
@@ -1,7 +1,22 @@
 var api = (function() {
 
+
+    function getSessionData(name, cb) {
+
+        $.ajax({
+            type: 'POST',
+            url: '/getToken',
+            data: JSON.stringify({
+                name: name
+            }),
+            success: cb,
+            contentType: 'application/json'
+        });
+
+    }
+
     return {
-        getSessionData: function(name) { return axios.post('/getToken', {name: name});}
+        getSessionData: getSessionData
     };
 
 })();
